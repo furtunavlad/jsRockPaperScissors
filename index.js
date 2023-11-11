7e7eb566679520c9f2f1737766ef6e598c89d0db
Vlad Furtuna <144479033+furtunavlad@users.noreply.github.com>
2023-11-11 02:30:19 +0200

    final version

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
-let computerMove = '';
+let score = JSON.parse(localStorage.getItem('score')) || {
+    wins: 0,
+    losses: 0,
+    ties: 0,
+};
 
-let score = JSON.parse(localStorage.getItem('score'));
-
-if(score == null){
-    score = {
-        wins: 0,
-        losses: 0,
-        ties: 0,
-    }
-}
+updateScoreElement();
 
 function playGame(playerMove){
 
-    pickComputerMove();
+    const computerMove = pickComputerMove();
 
     result = '';
     if (playerMove == 'scissors'){
         if (computerMove == 'rock'){
-            result = 'You lost.';
+            result = 'You lose.';
         } else if (computerMove == 'paper'){
             result = 'You win.';
         } else if (computerMove == 'scissors')
@@ -54,11 +50,24 @@ function playGame(playerMove){
 
     localStorage.setItem('score', JSON.stringify(score));
 
-    alert(`You picked ${playerMove}. Computer picked ${computerMove}. ${result}
-Wins: ${score.wins}, Ties: ${score.ties}, Losses: ${score.losses}`);
+    updateScoreElement();
+
+    document.querySelector('.js-result').innerHTML = result;
+
+    document.querySelector('.js-moves').innerHTML = `You
+    <img src="images/${playerMove}-emoji.png" class="move-icon">
+    <img src="images/${computerMove}-emoji.png" class="move-icon">
+    Computer`;
+}
+
+function updateScoreElement(){
+    document.querySelector('.js-score')
+        .innerHTML = `Wins: ${score.wins}, Ties: ${score.ties}, Losses: ${score.losses}`;
 }
 
 function pickComputerMove() {
+    let computerMove = '';
+
     const randomNumber = Math.random();
 
     if (randomNumber >= 0 && randomNumber < 1/3){
@@ -68,4 +77,5 @@ function pickComputerMove() {
     } else if (randomNumber >= 2/3 && randomNumber < 1){
         computerMove = 'scissors';
     }
+    return computerMove;
 }
